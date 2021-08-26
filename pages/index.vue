bd97bd96ac3e8b71bb6631fcb42df43a1b176acb
flukkyz555 <kunakorn.flukky@gmail.com>
2021-08-27 01:12:08 +0700

    testr

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,33 @@
   <div>
     <h1 class="display-1 text-center mt-10">
       CMU Line
+      <br>
     </h1>
+    <pre v-if="q">
+        {{ q }}
+        {{ $route.query['liff.state'] }}
+      </pre>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'empty'
+  layout: 'empty',
+  data () {
+    return {
+      q: null
+    }
+  },
+  mounted () {
+    this.q = this.$route.query
+    setTimeout(() => {
+      if (this.$route.query['liff.state'] === '/apps') {
+        this.$router.push({ name: 'apps' })
+      } else if (this.$route.query['liff.state'] === '/profile') {
+        this.$router.push({ name: 'profile' })
+      }
+    }, 2000)
+  }
+
 }
 </script>
