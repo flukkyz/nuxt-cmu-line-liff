5649342b11dac602e8c36812b299a72cd4aac976
flukkyz555 <kunakorn.flukky@gmail.com>
2021-09-29 23:04:45 +0700

    bug

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -30,23 +30,23 @@ export default {
   created () {
     // this.$breadcrumbs.clear()
     this.$store.commit('setPendingLogin', true)
-  },
-  async beforeMount () {
-    const timeOfTimeout = 20
-    const timeout = setTimeout(() => {
-      this.$store.commit('setPendingLogin', false)
-      this.$nuxt.error({ statusCode: 401, message: 'Unauthorized' })
-    }, timeOfTimeout * 1000)
-    const acc = await this.$axios.$get(`${process.env.baseUrl}/oauth2/callback?code=${this.$route.query.code}&state=${this.$route.query.state}`)
-    this.$auth.setUserToken(acc.access_token, acc.refresh_token).then(async () => {
-      const user = await this.$auth.fetchUser()
-      console.log(user)
-      if (user) {
-        clearTimeout(timeout)
-      }
-      this.$store.commit('setPendingLogin', false)
-      // this.$bus.$emit('reset-side-menu')
-    })
   }
+  // async beforeMount () {
+  //   const timeOfTimeout = 20
+  //   const timeout = setTimeout(() => {
+  //     this.$store.commit('setPendingLogin', false)
+  //     this.$nuxt.error({ statusCode: 401, message: 'Unauthorized' })
+  //   }, timeOfTimeout * 1000)
+  //   const acc = await this.$axios.$get(`${process.env.baseUrl}/oauth2/callback?code=${this.$route.query.code}&state=${this.$route.query.state}`)
+  //   this.$auth.setUserToken(acc.access_token, acc.refresh_token).then(async () => {
+  //     const user = await this.$auth.fetchUser()
+  //     console.log(user)
+  //     if (user) {
+  //       clearTimeout(timeout)
+  //     }
+  //     this.$store.commit('setPendingLogin', false)
+  //     // this.$bus.$emit('reset-side-menu')
+  //   })
+  // }
 }
 </script>
