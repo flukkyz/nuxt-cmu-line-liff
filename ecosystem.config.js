d5b496664a8d9520f85d219d23de69f595f7c254
flukkyz555 <kunakorn.flukky@gmail.com>
2021-10-01 00:12:03 +0700

    add api again

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
+      name: 'cmu-line',
       exec_mode: 'cluster',
-      instances: 1, // Or a number of instances
+      instances: '1', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
+      exp_backoff_restart_delay: 100,
       args: 'start'
     }
   ]
