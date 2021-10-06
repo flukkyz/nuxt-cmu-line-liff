fbe89f57e6b43bfc00433bf7e3676d2b398b30e1
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-10-06 10:47:22 +0700

    test template

--- api/config/line-utility.js
+++ api/config/line-utility.js
@@ -273,6 +273,7 @@ module.exports = {
                 "label": "แสดงรายละเอียดเพิ่มเติม",
                 "uri": "https://liff.line.me/1656332858-DgV6jA5l"
               },
+              "style": "secondary",
               "height": "sm"
             }
           ]
