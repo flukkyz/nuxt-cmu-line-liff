9d9e490637a0b8085b1551138878034184842271
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-10-06 10:55:27 +0700

    test template

--- api/config/line-utility.js
+++ api/config/line-utility.js
@@ -262,53 +262,53 @@ module.exports = {
         }
       ]
     }
-    return {
-      "type": "flex",
-      "altText": "เงินเดือนปัจจุบันของคุณ",
-      "contents": {
-        "type": "bubble",
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "alignItems": "center",
-          "justifyContent": "center",
-          "contents": [
-            {
-              "type": "text",
-              "text": "เงินเดือนปัจจุบันของคุณ"
-            },
-            {
-              "type": "text",
-              "weight": "bold",
-              "contents": [
-                {
-                  "type": "span",
-                  "text": new Intl.NumberFormat('th-th').format(salary),
-                  "size": "3xl"
-                },
-                {
-                  "type": "span",
-                  "text": ' บาท',
-                  "size": "xl"
-                },
-              ]
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "แสดงรายละเอียดเพิ่มเติม",
-                "uri": "https://liff.line.me/1656332858-DgV6jA5l"
-              },
-              "style": "primary",
-              "color": "#0000ff",
-              offsetTop: '50px',
-              "height": "sm"
-            }
-          ]
-        }
-      }
-    }
+    // return {
+    //   "type": "flex",
+    //   "altText": "เงินเดือนปัจจุบันของคุณ",
+    //   "contents": {
+    //     "type": "bubble",
+    //     "body": {
+    //       "type": "box",
+    //       "layout": "vertical",
+    //       "alignItems": "center",
+    //       "justifyContent": "center",
+    //       "contents": [
+    //         {
+    //           "type": "text",
+    //           "text": "เงินเดือนปัจจุบันของคุณ"
+    //         },
+    //         {
+    //           "type": "text",
+    //           "weight": "bold",
+    //           "contents": [
+    //             {
+    //               "type": "span",
+    //               "text": new Intl.NumberFormat('th-th').format(salary),
+    //               "size": "3xl"
+    //             },
+    //             {
+    //               "type": "span",
+    //               "text": ' บาท',
+    //               "size": "xl"
+    //             },
+    //           ]
+    //         },
+    //         {
+    //           "type": "button",
+    //           "action": {
+    //             "type": "uri",
+    //             "label": "แสดงรายละเอียดเพิ่มเติม",
+    //             "uri": "https://liff.line.me/1656332858-DgV6jA5l"
+    //           },
+    //           "style": "primary",
+    //           "color": "#0000ff",
+    //           offsetTop: '50px',
+    //           "height": "sm"
+    //         }
+    //       ]
+    //     }
+    //   }
+    // }
   },
   test3: () => {
     return {
