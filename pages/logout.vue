7cf070a723167e15f543f24b694b2bf02da71f9d
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-10-06 15:23:11 +0700

    test template

---
+++ pages/logout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-btn @click="logout">
+      Log out
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      liff.logout()
+    }
+  }
+}
+</script>
