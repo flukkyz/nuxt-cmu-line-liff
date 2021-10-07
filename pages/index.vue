c37fa09a589662f868cb922cc262a696aed884ce
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-10-07 17:08:46 +0700

    update

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex h-100 fill">
+    <v-img src="/images/logo.png" contain max-width="250" class="my-auto mx-auto" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty'
+}
+</script>
+
+<style lang="scss" scoped>
+.fill{
+  min-height: 100vh;
+}
+</style>
