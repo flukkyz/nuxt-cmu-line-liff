d8809d82579acf4dcf69dc47d732adc328cc2edd
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-10-07 17:06:02 +0700

    test template

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'cmu-line',
-      port: 3000,
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
+      instances: '1', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       exp_backoff_restart_delay: 100,
       args: 'start'
