864b855aadd449e477f9f9a4ef9d7ead9579ddc5
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-10-07 14:16:38 +0700

    test template

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,14 @@
 <template>
   <div>
-    <p>Q</p>
-    <pre v-if="$route.query">
-      {{ $route.query }}
-    </pre>
-    <h1>Hello</h1>
-    <pre v-if="profile">
-      {{ profile }}
-    </pre>
-    <p>
-      call API : {{ callApi ? 'T' : 'F' }}
-    </p>
-    <pre v-if="datas">
-      {{ datas }}
-    </pre>
-    <p>authen</p>
-    <pre v-if="authen">
-      {{ authen }}
-    </pre>
+    <v-img src="/logo.png" contain />
   </div>
 </template>
 
 <script>
 export default {
   layout: 'empty',
-  data () {
-    return {
-      callApi: false,
-      datas: null,
-      profile: null,
-      authen: null
-    }
-  },
-  mounted () {
+  beforeMount () {
     liff.init({ liffId: '1656332858-DgV6jA5l' })
-    // this.q = this.$route.query
-    // liff.init({
-    //   liffId: '1656332858-DgV6jA5l'
-    // }).then(() => {
-    //   console.log('line')
-    //   if (liff.isLoggedIn()) {
-    //     console.log('login')
-    //     liff.getProfile().then(async (profile) => {
-    //       console.log('before profile')
-    //       this.profile = profile
-    //       const url = `https://mis-api.cmu.ac.th/mis/lineapp/api/users/${profile.userId}`
-    //       const acc = await this.$axios.$get(url)
-    //       this.callApi = true
-    //       this.datas = acc
-    //       console.log('api profile')
-    //       if (acc.status === 'ok') {
-    //         console.log('if DB')
-    //         if (this.$route.query['liff.state'] === '/apps') {
-    //           this.$router.push({ name: 'apps' })
-    //         } else if (this.$route.query['liff.state'] === '/profile') {
-    //           this.$router.push({ name: 'profile' })
-    //         }
-    //         console.log('else Redirect')
-    //       } else {
-    //         console.log('else DB')
-    //         const authen = await this.$axios.$get('https://mis-api.cmu.ac.th/mis/lineapp/authorize')
-    //         this.authen = authen
-    //         window.location = authen.data
-    //       }
-    //     })
-    //   } else {
-    //     liff.login()
-    //   }
-    // })
   }
 
 }
