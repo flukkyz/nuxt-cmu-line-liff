29d3894afc11818b0456378030bfa46e2d4a73e3
flukkyz555 <kunakorn.flukky@gmail.com>
2021-10-26 22:31:12 +0700

    layout

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -54,6 +54,7 @@ export default {
           liff.getProfile().then(async (profile) => {
             this.profile = profile
             const url = `${process.env.apiUrl}${process.env.apiDirectory}token/${this.$route.query.code}?lineid=${profile.userId}`
+            console.log(url)
             await this.$axios.$get(url)
             this.$router.push({ name: this.$route.query.state })
           })
