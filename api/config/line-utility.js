80c117e9b0040bc088942b10f69d158b82690736
flukkyz555 <kunakorn.flukky@gmail.com>
2021-11-01 01:32:29 +0700

    updaate hook

--- api/config/line-utility.js
+++ api/config/line-utility.js
@@ -521,35 +521,39 @@ module.exports = {
   },
   test3: () => {
     return {
-      "type": "carousel",
-      "contents": [
-        {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "First bubble"
-              }
-            ]
+      "type": "flex",
+      "altText": "this is a flex message",
+      "contents": {
+        "type": "carousel",
+        "contents": [
+          {
+            "type": "bubble",
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "First bubble"
+                }
+              ]
+            }
+          },
+          {
+            "type": "bubble",
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Second bubble"
+                }
+              ]
+            }
           }
-        },
-        {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "Second bubble"
-              }
-            ]
-          }
-        }
-      ]
+        ]
+      }
     }
   },
   test4: () => {
