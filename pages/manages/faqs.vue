76925293086aaa9a33faaeae3702e4d5664bd0c6
flukkyz555 <kunakorn.flukky@gmail.com>
2021-11-01 01:48:58 +0700

    updaate hook

--- pages/manages/faqs.vue
+++ pages/manages/faqs.vue
@@ -40,7 +40,7 @@
                   <v-list-item-content>
                     <v-list-item-title>
                       <span class="font-weight-bold">Question</span>
-                      <br>
+                      &nbsp;
                       {{ faq.question }}
                     </v-list-item-title>
                     <v-list-item-subtitle>
