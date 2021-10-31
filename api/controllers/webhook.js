a2d3070a48d050ccd1f119991cd4db76d8afefc6
flukkyz555 <kunakorn.flukky@gmail.com>
2021-11-01 02:02:42 +0700

    updaate hook

--- api/controllers/webhook.js
+++ api/controllers/webhook.js
@@ -56,7 +56,7 @@ module.exports = {
         } else if(msg === 'D') {
           resp.push(lineUtility.test4())
         } else {
-          resp.push(lineUtility.message(`${msg} ยังไม่มีนะ ...`))
+          resp.push(lineUtility.message(`เห้ย!! อย่าพิมพ์ไปเรื่องดิ`))
         }
       } else if(event.type === "postback") {
         const postback = JSON.parse('{"' + event.postback.data.replace(/&/g, '","').replace(/=/g,'":"') + '"}', (key, value) => {
