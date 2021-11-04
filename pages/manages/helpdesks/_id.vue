5f769c2272f49a2f65b8073b7555bac9d5054cd5
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-04 17:42:55 +0700

    ok

--- pages/manages/helpdesks/_id.vue
+++ pages/manages/helpdesks/_id.vue
@@ -18,8 +18,11 @@ export default {
       const data = await this.$axios.$get(`${this.api}/${this.$route.params.id}`)
       this.data = data.data
       await this.$axios.$put(`${this.api}/mode/${this.$route.params.id}`, {
-        mode: 'read',
+        mode: 'start',
         category_id: '6183732a6f433335811186c3'
+      })
+      await this.$axios.$put(`${this.api}/message/${this.$route.params.id}`, {
+        content: 'start start start start start start start start start start start start start start '
       })
     } catch (e) {
       this.$nuxt.error({ statusCode: e.response.status, message: e.response.data.message })
