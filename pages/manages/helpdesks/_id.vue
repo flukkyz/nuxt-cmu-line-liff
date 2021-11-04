2290f97a66eed7c23c45be1133c7117fa703570b
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-04 17:10:55 +0700

    ok

--- pages/manages/helpdesks/_id.vue
+++ pages/manages/helpdesks/_id.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
     helpdesks
+    {{ data }}
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      api: `${process.env.apiUrl}${process.env.apiDirectory}helpdesks`,
+      data: null
+    }
+  },
+  async mounted () {
+    try {
+      const data = await this.$axios.$get(`${this.api}/${this.$route.params.id}`)
+      this.data = data.data
+    } catch (e) {
+      this.$nuxt.error({ statusCode: e.response.status, message: e.response.data.message })
+    }
+  }
+}
+</script>
