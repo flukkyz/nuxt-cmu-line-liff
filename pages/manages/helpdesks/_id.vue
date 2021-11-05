2b49627adf2fc774b759187fd668db96c90671a0
flukkyz555 <kunakorn.flukky@gmail.com>
2021-11-06 02:54:43 +0700

    add category

--- pages/manages/helpdesks/_id.vue
+++ pages/manages/helpdesks/_id.vue
@@ -82,8 +82,7 @@
         <div v-for="msg in data.message" :key="`msg-${msg._id}`">
           <div class="d-flex">
             <v-card :id="`msg-list-${msg._id}`" class="grey lighten-4 mb-3 rounded" max-width="70%" :class="msg.is_admin ? 'ml-auto' : ''" elevation="0">
-              <v-spacer v-if="msg.is_admin" />
-              <v-card-title class="pb-0">
+              <v-card-title class="pb-0" :class="msg.is_admin ? 'ml-auto' : ''">
                 {{ msg.is_admin ? `Admin: ${$auth.user.firstname} ${$auth.user.lastname}` : `${data.user_detail[0].firstname} ${data.user_detail[0].lastname}` }}
               </v-card-title>
               <v-card-text class="pa-2">
