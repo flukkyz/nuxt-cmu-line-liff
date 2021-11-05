5982ca641b34528c4df2c23d9025f7ed0ce9ef18
flukkyz555 <kunakorn.flukky@gmail.com>
2021-11-05 23:16:22 +0700

    add category

--- pages/manages/helpdesks/_id.vue
+++ pages/manages/helpdesks/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <v-list>
+    <v-list>
       <v-list-item>
         <v-list-item-icon>
           <v-tooltip top>
@@ -8,46 +8,46 @@
               <v-icon
                 v-bind="attrs"
                 large
-                :color=" item.admin_reply ? 'primary' : 'success'"
+                :color=" data.admin_reply ? 'primary' : 'success'"
                 v-on="on"
               >
-                {{ item.admin_reply ? `${item.mode === 'start' ? 'fas' : 'far'} fa-comments` : `${item.mode === 'read' ? 'fas' : 'far'} fa-comment-alt` }}
+                {{ data.admin_reply ? `${data.mode === 'start' ? 'fas' : 'far'} fa-comments` : `${data.mode === 'read' ? 'fas' : 'far'} fa-comment-alt` }}
               </v-icon>
             </template>
-            <span>{{ item.admin_reply ? 'ต้องการให้ตอบกลับ' : 'ข้อเสนอแนะ' }}</span>
+            <span>{{ data.admin_reply ? 'ต้องการให้ตอบกลับ' : 'ข้อเสนอแนะ' }}</span>
           </v-tooltip>
         </v-list-item-icon>
         <v-list-item-content>
           <v-list-item-title class="title">
-            <v-badge v-if="item.mode === 'wait'" dot color="warning">
-              {{ `${item.user_detail[0].firstname} ${item.user_detail[0].lastname}` }}
+            <v-badge v-if="data.mode === 'wait'" dot color="warning">
+              {{ `${data.user_detail[0].firstname} ${data.user_detail[0].lastname}` }}
             </v-badge>
             <span v-else>
-              {{ `${item.user_detail[0].firstname} ${item.user_detail[0].lastname}` }}
+              {{ `${data.user_detail[0].firstname} ${data.user_detail[0].lastname}` }}
             </span>
           </v-list-item-title>
           <v-list-item-subtitle class="mt-1">
             <v-icon x-small>
               fas fa-sitemap
             </v-icon>
-            {{ item.user_detail[0].organizationname }}
+            {{ data.user_detail[0].organizationname }}
                     &nbsp;
             <v-icon x-small>
               far fa-envelope
             </v-icon>
-            {{ item.user_detail[0].email }}
+            {{ data.user_detail[0].email }}
           </v-list-item-subtitle>
           <v-list-item-subtitle class="info--text mt-3 caption">
-            {{ item.message[0].content.length > 100 ? `${item.message[0].content.substring(0, 100)}...` : item.message[0].content }}
+            {{ data.message[0].content.length > 100 ? `${data.message[0].content.substring(0, 100)}...` : data.message[0].content }}
           </v-list-item-subtitle>
         </v-list-item-content>
         <v-list-item-action class="align-self-start">
           <p class="grey--text caption mb-0">
-            {{ $dateText(item.createdAt,'medium','long') }}
+            {{ $dateText(data.createdAt,'medium','long') }}
           </p>
         </v-list-item-action>
       </v-list-item>
-    </v-list> -->
+    </v-list>
 
     <pre>
     {{ data }}
@@ -66,7 +66,7 @@ export default {
   async mounted () {
     try {
       const data = await this.$axios.$get(`${this.api}/${this.$route.params.id}`)
-      this.data = data.data
+      this.data = data.data[0]
       console.log(this.data)
       await this.$axios.$put(`${this.api}/mode/${this.$route.params.id}`, {
         mode: 'wait',
