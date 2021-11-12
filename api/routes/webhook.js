8018b1ebb6d92298755c39e8bceb822de8018ce4
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-12 14:42:21 +0700

    sws

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -6,9 +6,11 @@ const router = express.Router()
 const path = '/webhook'
 
 router.get(`${path}/`, (req,res) => {
+  console.log('dddddget');
 res.send('111111111111111')
 })
 router.post(`${path}/`, (req,res) => {
+  console.log('ddddd');
 res.json({a:111})
 })
 
