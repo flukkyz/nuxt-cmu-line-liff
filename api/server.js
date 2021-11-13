fd6e62bd20a98f0cc13c8031208b3a122e411107
flukkyz555 <kunakorn.flukky@gmail.com>
2021-11-13 14:57:46 +0700

    t

--- api/server.js
+++ api/server.js
@@ -1,17 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const ws = require('ws');
 
-var wsClient = new ws('wss://mis-api.cmu.ac.th/mis/lineapp/ws/api', 'protocol')
 const { notFound, handleError } = require('./middlewares')
 const routes = require('./routes')
 
 const app = express()
-
-global.wsClient.onopen = (event) => {
-  console.log('Successfully connected to the echo websocket server...')
-}
 
 app.use(express.json({limit: '50mb'}))
 app.use(cors())
