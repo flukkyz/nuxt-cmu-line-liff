327bcc2b960d5771092a013a37dba9e518c26b05
flukkyz555 <kunakorn.flukky@gmail.com>
2021-11-15 22:19:20 +0700

    ok

--- pages/liff/helpdesk.vue
+++ pages/liff/helpdesk.vue
@@ -149,7 +149,7 @@ export default {
       console.log([...send.data.admin])
       await this.pushMessage([this.profile.userId], `เราได้รับข้อความการแจ้งปัญหาการใช้งานของคุณแล้ว ${this.form.admin_reply ? 'เจ้าหน้าที่จะตอบกลับโดยเร็วที่สุด' : ''} ขอบคุณครับ`)
       setTimeout(async () => {
-        await this.pushMessage([...send.data.admin], `มีข้อความการแจ้งปัญหาการใช้งานใหม่${this.form.admin_reply ? '(ต้องการให้ตอบกลับ)' : ''}`)
+        await this.pushMessage(send.data.admin, `มีข้อความการแจ้งปัญหาการใช้งานใหม่${this.form.admin_reply ? '(ต้องการให้ตอบกลับ)' : ''}`)
       }, 100)
       this.close()
     },
