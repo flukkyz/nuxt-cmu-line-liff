90aea7f0f9eaa2340127fd9fb466d1c06bd96499
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-25 13:54:24 +0700

    bug

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const { webhookController } = require('../controllers')
 
+googleFinance = require('google-finance');
+
 const router = express.Router()
 
 const path = '/webhook'
 
 router.post(`${path}/`, webhookController.index)
+
+router.get((req,res) => {
+  googleFinance.companyNews({
+    symbol: 'SET:PLANB'
+  }, function (err, news) {
+    console.log(news);
+  });
+  res.send("HTTP POST request sent to the webhook URL!")
+})
 
 module.exports = router
