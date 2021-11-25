4d19c656d881e753ac53d23f5c8f53094173d767
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-25 16:48:52 +0700

    bug

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -1,34 +1,10 @@
 const express = require('express')
 const { webhookController } = require('../controllers')
 
-const axios = require('axios')
-
 const router = express.Router()
 
 const path = '/webhook'
 
 router.post(`${path}/`, webhookController.index)
-
-router.get(`${path}/test`,(req,res) => {
-  console.log('test ...............................');
-
-  var options = {
-    method: 'GET',
-    url: 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes',
-    params: {region: 'SG', symbols: 'thb=x'},
-    headers: {
-      'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
-      'x-rapidapi-key': 'a9b7101e0dmsh9d51c5f2ef8c0e6p17bf57jsnddb1d5cabb79'
-    }
-  };
-
-  axios.request(options).then(function (response) {
-    console.log(response.data.quoteResponse.result[0].regularMarketPrice);
-  }).catch(function (error) {
-    console.error(error);
-  });
-  
-  res.json({a: 1})
-})
 
 module.exports = router
