a788fcad7bd581efc0c06d15aa76cce68b168959
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-25 14:01:11 +0700

    bug

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -10,12 +10,12 @@ const path = '/webhook'
 router.post(`${path}/`, webhookController.index)
 
 router.get(`${path}/test`,(req,res) => {
-  googleFinance.companyNews({
-    symbol: 'NASDAQ:AAPL'
-  }, function (err, news) {
-    console.log(news);
-    // res.send("HTTP POST request sent to the webhook URL!")
-  });
+  // googleFinance.companyNews({
+  //   symbol: 'NASDAQ:AAPL'
+  // }, function (err, news) {
+  //   console.log(news);
+  //   // res.send("HTTP POST request sent to the webhook URL!")
+  // });
   res.json({a: 1})
 })
 
