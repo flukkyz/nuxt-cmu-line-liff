aa37f77978fd41c506cdd8a2669171d38a964e67
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-25 15:07:57 +0700

    bug

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { webhookController } = require('../controllers')
 
-const googleStocks = require('google-stocks');
+const axios = require('axios')
 
 const router = express.Router()
 
@@ -11,17 +11,24 @@ router.post(`${path}/`, webhookController.index)
 
 router.get(`${path}/test`,(req,res) => {
   console.log('test ...............................');
-  googleStocks(['AAPL'])
-  .then((data) => {
-    console.log(data);
-    res.json({a: 1})
-  })
-  .catch((error) => {
-    console.log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
-    console.log(error);
-    res.json({a: 1})
-  });
 
+  var options = {
+    method: 'GET',
+    url: 'https://yh-finance.p.rapidapi.com/auto-complete',
+    params: {q: 'tesla', region: 'US'},
+    headers: {
+      'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
+      'x-rapidapi-key': 'a9b7101e0dmsh9d51c5f2ef8c0e6p17bf57jsnddb1d5cabb79'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+  
+  res.json({a: 1})
 })
 
 module.exports = router
