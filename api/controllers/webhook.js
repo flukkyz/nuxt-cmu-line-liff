cedad8fc83d22a7decf2e7460713fb4533bc3e35
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-11-25 16:42:15 +0700

    bug

--- api/controllers/webhook.js
+++ api/controllers/webhook.js
@@ -147,7 +147,7 @@ module.exports = {
                 console.log(`${coin}.bk`);
                 yahooFinanceOptions.params.symbols = `${coin}.bk`
                 const setBK = await axios.request(yahooFinanceOptions)
-                console.log(setBK);
+                console.log(setBK.data);
                 hasResult = true  
                 const symbol = setBK.data.quoteResponse.result[0].symbol.split('.')[0]
                 const name  = setBK.data.quoteResponse.result[0].longName
